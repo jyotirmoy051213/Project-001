a225757cc60a7039a5513a0d94df083228f1164b
jyotirmoy051213 <g.jyotirmoy051213@gmail.com>
2025-10-02 00:25:47 +0600

    Fixed some bugs

--- MyDeepSeekCrawlProject/crawl_schema.py
+++ MyDeepSeekCrawlProject/crawl_schema.py
@@ -30,7 +30,7 @@ async def crawl_products():
     try:
         async with AsyncWebCrawler(config=browser_config) as crawler:
             while True:
-                url = f"https://www.ryans.com/category/laptop?page={page_number}" # URL with pagination
+                url = f"https://www.ryans.com/category/desktop-and-server?page={page_number}" # URL with pagination
                 css_selector= ".card.h-100" # Only select where class contains this string
 
                 # Build schema for JsonCssExtractionStrategy by inspecting the webpage
@@ -40,8 +40,8 @@ async def crawl_products():
                     "fields": [
                         {"name": "name", "selector": ".card-text.p-0.m-0.list-view-text a", "type": "attribute", "attribute": "data-bs-original-title"},
                         {"name": "image_url", "selector": ".image-box img", "type": "attribute", "attribute": "src"},
-                        {"name": "description", "selector": ".short-desc-attr li", "type": "key-value", "split": " - "},
-                        {"name": "price", "selector": ".card-body.text-center a", "type": "text"},
+                        {"name": "description", "selector": ".short-desc-attr li", "type": "list", "fields": [{"name": "feature","type": "text"}]},
+                        {"name": "price", "selector": ".pr-text.cat-sp-text.pb-1.text-dark.text-decoration-none", "type": "text"},
                         {"name": "url", "selector": ".card-text.p-0.m-0.list-view-text a", "type": "attribute", "attribute": "href"}
                     ] 
                 }
@@ -93,18 +93,29 @@ async def crawl_products():
                 
                 print(f"Proceeding to Page after {delay_time} seconds...")
                 await asyncio.sleep(delay_time) # Be polite and avoid overwhelming the server
+                break
 
 
     finally: # CSV will be saved even if error occurs
         await crawler.close()    
         # Save to CSV once after all pages are crawled
-        csv_file = "D:/My Codes/Projects/scraping/MyDeepSeekCrawlProject/products.csv"
-        with open (csv_file, mode='w', newline='', encoding='utf-8') as file:
+        
+
+        csv_file = "D:/My Codes/Projects/scraping/MyDeepSeekCrawlProject/Ryans_products.csv"
+        headers = [field['name'] for field in schema['fields']]
+        # Check if file already exists to write header only once
+        file_exists = os.path.isfile(csv_file)
+
+        with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            headers = list(schema['fields'][i]['name'] for i in range(len(schema['fields'])))
-            writer.writerow(headers)
+            
+            # Write header only if file is new
+            if not file_exists:
+                writer.writerow(headers)
+            
             for product in all_products:
                 writer.writerow([product.get(field, "") for field in headers])
+
 
         
         print(f"Saved {len(all_products)} unique products to {csv_file}.")
