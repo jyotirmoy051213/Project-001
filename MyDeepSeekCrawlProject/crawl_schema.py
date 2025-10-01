6bbdc2e9ff1812ecb9b34851a206e34d587e5bc0
jyotirmoy051213 <g.jyotirmoy051213@gmail.com>
2025-10-02 05:35:53 +0600

    Somewhat automated. Done with Ryans Products_02/10/2025

--- MyDeepSeekCrawlProject/crawl_schema.py
+++ MyDeepSeekCrawlProject/crawl_schema.py
@@ -40,38 +40,41 @@ async def crawl_products():
     session_id = 'product_crawl_session'
 
     # initialize state variables
+    crawl_number = 14 # Change this to crawl different categories from base_urls list
     page_number = 1 # Start from page 1
     all_products = [] # List to hold all scraped products
     seen_names = set() # Set to track unique products
-    delay_time = 5 # Delay between requests to avoid overwhelming the server + Rate limiting
+    delay_time = 2 # Delay between requests to avoid overwhelming the server + Rate limiting
 
     # URLS of ryanscomputerbd
     base_urls = [
-        {"category": "laptop", "base_url": "https://www.ryans.com/category/laptop"},
-        {"category": "desktop_and_server", "base_url": "https://www.ryans.com/category/desktop-and-server"},
-        {"category": "gaming", "base_url": "https://www.ryans.com/category/gaming"},
-        {"category": "monitor", "base_url": "https://www.ryans.com/category/monitor"},
-        {"category": "tablet_pc", "base_url": "https://www.ryans.com/category/tablet"},
-        {"category": "printer", "base_url": "https://www.ryans.com/category/printer"},
-        {"category": "camera", "base_url": "https://www.ryans.com/category/camera"},
-        {"category": "security", "base_url": "https://www.ryans.com/category/security-system"},
-        {"category": "network", "base_url": "https://www.ryans.com/category/network"},
-        {"category": "sound", "base_url": "https://www.ryans.com/category/sound-system"},
-        {"category": "office_items", "base_url": "https://www.ryans.com/category/office-items"},
-        {"category": "accessories", "base_url": "https://www.ryans.com/category/accessories"},
-        {"category": "software", "base_url": "https://www.ryans.com/category/software"},
-        {"category": "gadget", "base_url": "https://www.ryans.com/category/gadget"},
-        {"category": "store", "base_url": "https://www.ryans.com/category/store"}
+        {"category": "laptop", "base_url": "https://www.ryans.com/category/laptop"}, #0
+        {"category": "desktop_and_server", "base_url": "https://www.ryans.com/category/desktop-and-server"}, #1
+        {"category": "gaming", "base_url": "https://www.ryans.com/category/gaming"}, #2
+        {"category": "monitor", "base_url": "https://www.ryans.com/category/monitor"}, #3
+        {"category": "tablet_pc", "base_url": "https://www.ryans.com/category/tablet"}, #4
+        {"category": "printer", "base_url": "https://www.ryans.com/category/printer"}, #5
+        {"category": "camera", "base_url": "https://www.ryans.com/category/camera"}, #6
+        {"category": "security", "base_url": "https://www.ryans.com/category/security-system"}, #7
+        {"category": "network", "base_url": "https://www.ryans.com/category/network"}, #8
+        {"category": "sound", "base_url": "https://www.ryans.com/category/sound-system"}, #9
+        {"category": "office_items", "base_url": "https://www.ryans.com/category/office-items"}, #10
+        {"category": "accessories", "base_url": "https://www.ryans.com/category/accessories"}, #11
+        {"category": "software", "base_url": "https://www.ryans.com/category/software"}, #12
+        {"category": "gadget", "base_url": "https://www.ryans.com/category/gadget"}, #13
+        {"category": "store", "base_url": "https://www.ryans.com/category/store"} #14
     ]
 
     # Start the crawler, TRY to ensure proper cleanup if crawler fails
     try:
         async with AsyncWebCrawler(config=browser_config) as crawler:
             while True:
-                url = f"https://www.ryans.com/category/laptop?page={page_number}" # URL with pagination
+                category = base_urls[crawl_number]["category"] # Change category as needed by crawl_number
+                print(f"\nStatus: Crawling. Crawl Number: {crawl_number}. Category: {category}. Page: {page_number}") # Log current status
+                url = f"{base_urls[crawl_number]["base_url"]}?page={page_number}" # URL with pagination
                 css_selector= ".card.h-100" # Only select where class contains this string
 
-                category = "laptop" # Change category as needed
+                
 
                 # Build schema for JsonCssExtractionStrategy by inspecting the webpage
                 schema = {
@@ -120,12 +123,12 @@ async def crawl_products():
                 for item in extracted_data:
                     item['category'] = category # Add category field
                     all_products.append(item)
-                print(f"Page {page_number}: added {len(extracted_data)} products, Total so far: {len(all_products)}.")
-
+                print(f"Page {page_number}: added {len(extracted_data)} products. \nTotal added in current crawl: {len(all_products)}.")
+                #break # For testing, comment out to crawl all pages
                 page_number +=1
             
                 # Delay Log
-                print(f"Proceeding to Page after {delay_time} seconds...")
+                print(f"Proceeding to next page after {delay_time} seconds...")
                 await asyncio.sleep(delay_time) # Be polite and avoid overwhelming the server
 
     # CSV will be saved even if error occurs
@@ -133,7 +136,9 @@ async def crawl_products():
         await crawler.close()    
         
         # Save to CSV once after all pages are crawled
-        csv_file = "D:/My Codes/Projects/scraping/MyDeepSeekCrawlProject/Ryans_products.csv"
+        csv_file = "D:/My Codes/Projects/scraping/MyDeepSeekCrawlProject/Ryans_products.csv" # Main file
+        #csv_file = "D:/My Codes/Projects/scraping/MyDeepSeekCrawlProject/trial_Ryans_products.csv" # Trial file for testing
+        
         headers = ['category'] + [field['name'] for field in schema['fields']]
         # Check if file already exists to write header only once
         file_exists = os.path.isfile(csv_file)
@@ -153,7 +158,7 @@ async def crawl_products():
 
         
         # Final Log
-        print(f"Saved {len(all_products)} unique products to {csv_file}.")
+        print(f"Added {len(all_products)} new products to database.")
 
 
         
