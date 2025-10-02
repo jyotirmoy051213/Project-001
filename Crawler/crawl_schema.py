af2623a3fa5cb42a6fae56fe656d7fbc565cd081
jyotirmoy051213 <g.jyotirmoy051213@gmail.com>
2025-10-02 20:42:17 +0600

    Modularization update: Separate Site_config file

--- Crawler/crawl_schema.py
+++ Crawler/crawl_schema.py
@@ -9,10 +9,10 @@ Code need to be modularized.
 
 """
 SCOPE FOR IMPROVEMENTS:
-1. Could be automated to loop through all categories in base_urls list.
+1. Could be automated to loop through all categories in urls_to_crawl list.
 2. CSV is written once after all, could be optimized to write in each page.
 3. Some functions can be made to shorten the code.
-4. Separate scripts can be made for the code, especially Schema (as it is website-specific), Configs (Extraction Strategy), base_urls, logging, writing files and main logic.
+4. Separate scripts can be made for the code, especially Schema (as it is website-specific), Configs (Extraction Strategy), urls_to_crawl, logging, writing files and main logic.
 5. Error handling might need improvements.
 6. Duplicates are not handled.
 7. Logging needs to be improved. Logs needs to be written to a file. Explore logging module.
@@ -32,6 +32,7 @@ from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, BrowserConfig, JsonCssEx
 from dotenv import load_dotenv
 import json
 import csv
+import site_config_ryans
 
 load_dotenv()
 
@@ -46,67 +47,38 @@ async def crawl_products():
     session_id = 'product_crawl_session'
 
     # initialize state variables
-    crawl_number = 14 # Change this to crawl different categories from base_urls list
+    crawl_number = 0 # Change this to crawl different categories from urls_to_crawl list
     page_number = 1 # Start from page 1
     all_products = [] # List to hold all scraped products
     seen_names = set() # Set to track unique products
     delay_time = 2 # Delay between requests to avoid overwhelming the server + Rate limiting
 
-    # URLS of ryanscomputerbd
-    base_urls = [
-        {"category": "laptop", "base_url": "https://www.ryans.com/category/laptop"}, #0
-        {"category": "desktop_and_server", "base_url": "https://www.ryans.com/category/desktop-and-server"}, #1
-        {"category": "gaming", "base_url": "https://www.ryans.com/category/gaming"}, #2
-        {"category": "monitor", "base_url": "https://www.ryans.com/category/monitor"}, #3
-        {"category": "tablet_pc", "base_url": "https://www.ryans.com/category/tablet"}, #4
-        {"category": "printer", "base_url": "https://www.ryans.com/category/printer"}, #5
-        {"category": "camera", "base_url": "https://www.ryans.com/category/camera"}, #6
-        {"category": "security", "base_url": "https://www.ryans.com/category/security-system"}, #7
-        {"category": "network", "base_url": "https://www.ryans.com/category/network"}, #8
-        {"category": "sound", "base_url": "https://www.ryans.com/category/sound-system"}, #9
-        {"category": "office_items", "base_url": "https://www.ryans.com/category/office-items"}, #10
-        {"category": "accessories", "base_url": "https://www.ryans.com/category/accessories"}, #11
-        {"category": "software", "base_url": "https://www.ryans.com/category/software"}, #12
-        {"category": "gadget", "base_url": "https://www.ryans.com/category/gadget"}, #13
-        {"category": "store", "base_url": "https://www.ryans.com/category/store"} #14
-    ]
+    # Website variables from site_configuration: Category of products, URLs & Schema
+    urls_to_crawl = site_config_ryans.URLS_TO_CRAWL
+    schema = site_config_ryans.SCHEMA_FOR_EXTRACTION
+    css_selector = site_config_ryans.CSS_SELECTOR
 
     # Start the crawler, TRY to ensure proper cleanup if crawler fails
     try:
         async with AsyncWebCrawler(config=browser_config) as crawler:
             while True:
-                category = base_urls[crawl_number]["category"] # Change category as needed by crawl_number
+                category = urls_to_crawl[crawl_number]["category"] # Change category as needed by crawl_number
                 print(f"\nStatus: Crawling. Crawl Number: {crawl_number}. Category: {category}. Page: {page_number}") # Log current status
-                url = f"{base_urls[crawl_number]["base_url"]}?page={page_number}" # URL with pagination
-                css_selector= ".card.h-100" # Only select where class contains this string
-
-                
-
-                # Build schema for JsonCssExtractionStrategy by inspecting the webpage
-                schema = {
-                    "name": "Product",
-                    "baseSelector": ".card.h-100",
-                    "fields": [
-                        {"name": "name", "selector": ".card-text.p-0.m-0.list-view-text a", "type": "attribute", "attribute": "data-bs-original-title"},
-                        {"name": "image_url", "selector": ".image-box img", "type": "attribute", "attribute": "src"},
-                        {"name": "description", "selector": ".short-desc-attr li", "type": "list", "fields": [{"name": "feature","type": "text"}]},
-                        {"name": "price", "selector": ".pr-text.cat-sp-text.pb-1.text-dark.text-decoration-none", "type": "text"},
-                        {"name": "url", "selector": ".card-text.p-0.m-0.list-view-text a", "type": "attribute", "attribute": "href"}
-                    ] 
-                }
-                
+                base_url = f"{urls_to_crawl[crawl_number]["base_url"]}?page={page_number}" # URL with pagination copied from site_config
+                    
+        
                 # Configure Crawler
-                config=CrawlerRunConfig(
+                crawler_config=CrawlerRunConfig(
                     session_id=session_id, # Unique session ID for the crawl
                     extraction_strategy=JsonCssExtractionStrategy(schema=schema),
-                    css_selector=css_selector,
+                    css_selector=css_selector,  # Decides what part of an encountered webpage crawler will crawl through
                     cache_mode=CacheMode.BYPASS
                     )
 
                 # Crawl the page    
                 result = await crawler.arun(
-                    url=url,
-                    config=config    
+                    url=base_url,
+                    config=crawler_config    
                 )
                 
 
--- Crawler/site_config_ryans.py
+++ Crawler/site_config_ryans.py
@@ -1,3 +1,44 @@
 """
-
+This script has Website variables for ryanscomputerbd.com
+urls_to_crawl --> Categories of products and URLs to go through in the website 
+schema --> Handcrafted JSON Schema of Ryans website (Upto 01/10/2025) to extract from crawled content - LLM-free extraction strategy
 """
+
+# List of product categories and their URLs
+URLS_TO_CRAWL = [
+        {"category": "laptop", "base_url": "https://www.ryans.com/category/laptop"}, #0
+        {"category": "desktop_and_server", "base_url": "https://www.ryans.com/category/desktop-and-server"}, #1
+        {"category": "gaming", "base_url": "https://www.ryans.com/category/gaming"}, #2
+        {"category": "monitor", "base_url": "https://www.ryans.com/category/monitor"}, #3
+        {"category": "tablet_pc", "base_url": "https://www.ryans.com/category/tablet"}, #4
+        {"category": "printer", "base_url": "https://www.ryans.com/category/printer"}, #5
+        {"category": "camera", "base_url": "https://www.ryans.com/category/camera"}, #6
+        {"category": "security", "base_url": "https://www.ryans.com/category/security-system"}, #7
+        {"category": "network", "base_url": "https://www.ryans.com/category/network"}, #8
+        {"category": "sound", "base_url": "https://www.ryans.com/category/sound-system"}, #9
+        {"category": "office_items", "base_url": "https://www.ryans.com/category/office-items"}, #10
+        {"category": "accessories", "base_url": "https://www.ryans.com/category/accessories"}, #11
+        {"category": "software", "base_url": "https://www.ryans.com/category/software"}, #12
+        {"category": "gadget", "base_url": "https://www.ryans.com/category/gadget"}, #13
+        {"category": "store", "base_url": "https://www.ryans.com/category/store"} #14
+    ]
+
+# Handcrafted schema for JsonCssExtractionStrategy by inspecting the webpage
+SCHEMA_FOR_EXTRACTION = {
+        "name": "Product",
+        "baseSelector": ".card.h-100",            
+        "fields": [
+            {"name": "name", "selector": ".card-text.p-0.m-0.list-view-text a", "type": "attribute", "attribute": "data-bs-original-title"},
+            {"name": "image_url", "selector": ".image-box img", "type": "attribute", "attribute": "src"},
+            {"name": "description", "selector": ".short-desc-attr li", "type": "list", "fields": [{"name": "feature","type": "text"}]},
+            {"name": "price", "selector": ".pr-text.cat-sp-text.pb-1.text-dark.text-decoration-none", "type": "text"},
+            {"name": "url", "selector": ".card-text.p-0.m-0.list-view-text a", "type": "attribute", "attribute": "href"}
+            ] 
+        }
+
+# base_url_format:: Copy and paste as "base_url" variable for ryans website
+# base_url = f"{urls_to_crawl[crawl_number]["base_url"]}?page={page_number}"
+
+
+# CSS Selector for crawler
+CSS_SELECTOR = ".card.h-100"
