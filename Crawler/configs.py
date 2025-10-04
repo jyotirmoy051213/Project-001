d28267817681a28d9c5b343712c24770cb96e5b8
jyotirmoy051213 <g.jyotirmoy051213@gmail.com>
2025-10-05 02:37:21 +0600

    MAKING CRAWLER TASK-AGNOSTIC

--- Crawler/configs.py
+++ Crawler/configs.py
@@ -6,9 +6,20 @@ import os, csv
 from pydantic import BaseModel
 from crawl4ai import BrowserConfig, CrawlerRunConfig, LLMConfig, LLMExtractionStrategy, JsonCssExtractionStrategy, CacheMode
 
+## CONTROL VARIABLES
 TEST_MODE = True
-MAIN_FILE= "D:/My Codes/Projects/Project-001/Crawler/Database/Computer-village_products.csv"
-TEST_FILE= "D:/My Codes/Projects/Project-001/Crawler/test_csv.csv"
+CRAWL_NUMBER = 0
+PAGE_NUMBER = 1
+DELAY_TIME = 5
+
+
+MAIN_FILE= "D:/My Codes/Projects/Project-001/Database/Computer-village_products.csv"
+TEST_FILE= "D:/My Codes/Projects/Project-001/Crawler/trials/test_csv.csv"
+SESSION_ID = "project-001"
+
+
+
+## STRATEGY
 CSS_SELECTOR = ".main-products.product-grid"
 URLS_TO_CRAWL = [
   { "category": "Laptop", "base_url": "https://www.computervillage.com.bd/laptop-notebook" },
@@ -28,25 +39,10 @@ URLS_TO_CRAWL = [
   { "category": "Server & Storage", "base_url": "https://www.computervillage.com.bd/server-and-storage" },
   { "category": "Pre Owned", "base_url": "https://www.computervillage.com.bd/pre-owned" }
 ]
+CATEGORY = URLS_TO_CRAWL[CRAWL_NUMBER]['category'] 
 
-## CONFIGURATION SETTINGS
-def get_browser_config():
-    return BrowserConfig(
-        browser_type='chromium', # Chrome Browser
-        headless=False, # Headless == No GUI
-        verbose=True # Verbose logging
-    ) 
 
-def get_crawler_config(session_id, css_selector, schema): # ARGUMENTS THAT NEED TO BE PASSED FROM THE MAIN SCRIPT, MUST BE DEFINED BEFORE EXECUTING THIS FUNCTION
-    return CrawlerRunConfig(
-        session_id=session_id, # Unique session ID for the crawl
-        extraction_strategy=JsonCssExtractionStrategy(schema=schema),
-        css_selector=css_selector,  # Decides what part of an encountered webpage crawler will crawl through
-        cache_mode=CacheMode.BYPASS
-    )
-
-## SCHEMA FOR EXTRACTION
-# Schema for JsonCssExtractionStrategy by inspecting the webpage
+# SCHEMA FOR JsonCSSExtraction STRATEGY
 SCHEMA_FOR_EXTRACTION = {
         "name": "Product",
         "baseSelector": ".product-thumb",  # A Selector which is repeated, and contains information of a single product
@@ -59,9 +55,8 @@ SCHEMA_FOR_EXTRACTION = {
             ] 
         }
 
-# Pydantic Basemodel for LLM-based Extraction Strategy
+# PYDANTIC SCHEMA FOR LLM-BASED EXTRACTION STRATEGY
 class Products(BaseModel):
-    # Data structure for extracted product information
     category : str
     name : str
     image_url : str
@@ -70,23 +65,50 @@ class Products(BaseModel):
     url : str
 
 
+## CONFIGURATION SETTINGS
+def get_browser_config():
+    return BrowserConfig(
+        browser_type='chromium', # Chrome Browser
+        headless=False, # Headless == No GUI
+        verbose=True # Verbose logging
+    ) 
+
+def get_crawler_config(): # ARGUMENTS THAT NEED TO BE PASSED FROM THE MAIN SCRIPT, MUST BE DEFINED BEFORE EXECUTING THIS FUNCTION
+    return CrawlerRunConfig(
+        session_id=SESSION_ID, # Unique session ID for the crawl
+        extraction_strategy=JsonCssExtractionStrategy(schema=SCHEMA_FOR_EXTRACTION),
+        css_selector=CSS_SELECTOR,  # Decides what part of an encountered webpage crawler will crawl through
+        cache_mode=CacheMode.BYPASS
+    )
+
+# CRAWL THE URL
+async def crawl_and_extract_from_page(url, crawler):
+    result = await crawler.arun(
+        url=url,
+        configs=get_crawler_config()
+    )
+    return result
+
+
+
 ## WRITING
 HEADERS = ['category'] + [field['name'] for field in SCHEMA_FOR_EXTRACTION['fields']]
 
-def custom_csv_writer(file_to_be_written, headers, products):    
-    file_exists = os.path.isfile(file_to_be_written)            # Write to CSV
+def custom_csv_writer(file_to_be_written, products):    
+    file_exists = os.path.isfile(file_to_be_written)
     with open(file_to_be_written, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
 
         # Write header only if file is new, otherwise assume, there is already a header
         if not file_exists:
-            writer.writerow(headers)
+            writer.writerow(HEADERS)
         
         # THIS SECTION IS EXTRACTION SPECIFIC::: Write as per extracted data
         for product in products:
+            product['category'] = CATEGORY
             price = product.get("price", "")
             if price and price.startswith("Ex Tax:"):
                 product["price"] = price.replace("Ex Tax:", "").strip()
 
-            row = [product.get('category', '')] + [product.get(field, '') for field in headers[1:]]
+            row = [product.get('category', '')] + [product.get(field, '') for field in HEADERS[1:]]
             writer.writerow(row)
--- Crawler/main.py
+++ Crawler/main.py
@@ -8,8 +8,6 @@ Schema is handcrafted after inspecting the webpage structure for specific websit
 """
 SCOPE FOR IMPROVEMENTS:
 1. Could be automated to loop through all categories in urls_to_crawl list.
-2. CSV is written once after all, could be optimized to write in each page.
-3. Writer function can be added.
 4. Separate scripts can be made for the code, especially logging, writing files and main logic.
 5. Error handling might need improvements.
 6. Duplicates are not handled.
@@ -19,7 +17,6 @@ SCOPE FOR IMPROVEMENTS:
 10. Build in Docker.
 11. Explore SQL DB instead of CSV.
 12. Need to explore LLM based schema generation.
-13. Need to setup a requirements.txt file.
 """
 
 import asyncio, json
@@ -32,115 +29,94 @@ test_mode = configs.TEST_MODE
 
 async def crawl_products(test_mode=False):
     # CONFIGURATION VARIABLES TO SET UP CRAWLING
-    urls_to_crawl = configs.URLS_TO_CRAWL
-    schema = configs.SCHEMA_FOR_EXTRACTION
-    css_selector = configs.CSS_SELECTOR
-    headers = configs.HEADERS # HEADERS IS NEEDED AS EXTRACTED CONTENT DOES NOT INCLUDE HEADER
+    urls_to_crawl = configs.URLS_TO_CRAWL 
     browser_config = configs.get_browser_config()   
     
 
     # VARIABLES TO DICTATE CRAWLING
-    crawl_number = 0 # Change this to crawl different categories from urls_to_crawl list
-    page_number = 1 # Start from page 1
-    
-    delay_time = 2 # Delay between requests to avoid overwhelming the server + Rate limiting
-    session_id = "crawling_session"
-    
-    # OTHER VARIABLES 
-    product_count = 0 # Track product number
-    crawled_page_count = 0
-    seen_products = set() # Set to track unique products
-    category = urls_to_crawl[crawl_number]["category"] # Change category as needed by crawl_number
+    crawl_number = configs.CRAWL_NUMBER # Change this to crawl different categories from urls_to_crawl list
+    page_number = configs.PAGE_NUMBER # Start from page 1 
+    delay_time = configs.DELAY_TIME # Delay between requests to avoid overwhelming the server + Rate limiting
     main_file = configs.MAIN_FILE
     test_file = configs.TEST_FILE
 
     # Start the crawler, TRY to ensure proper cleanup if crawler fails
     try:
         async with AsyncWebCrawler(config=browser_config) as crawler:
+            product_count = 0
+            crawled_page_count = 0
+            seen_products = set()
             while True:
+                ## URL should be made Task Agnostic
                 url = f"{urls_to_crawl[crawl_number]['base_url']}?page={page_number}" # URL with pagination copied from site_config
                 # Log current status
-                print(f"\nStatus: Crawling, Crawl No: {crawl_number}/{len(urls_to_crawl)-1}, Category: {category}, Page: {page_number}") # Log current status
+                print("STATUS: INITIATING CRAWLING.")
                 
-                # Configure Crawler
-                crawler_config = configs.get_crawler_config(
-                    session_id=session_id,
-                    css_selector=css_selector,
-                    schema=schema
-                )
-
-                # Crawl the page    
-                result = await crawler.arun(
-                    url=url,
-                    config=crawler_config    
-                )
-
-                # Parse results safely, skip if JSON is invalid
-                try:
-                    extracted_data = json.loads(result.extracted_content)
-                # If encountered json decode error, skip this page
-                except json.JSONDecodeError:
-                    print(f"Error decoding JSON in Page {page_number}, skipping.")
-                    page_number += 1
-                    crawled_page_count += 1 
-                    continue 
-
-                # Stop if no products found
-                if not extracted_data:
-                    print(f"No products found in Page {page_number}.")
-                    break 
+                result = await configs.crawl_and_extract_from_page(url=url, crawler=crawler)
                 
-                # Append new unique products
-                new_products = [] 
-                for item in extracted_data:
-                    item['category'] = category # Add category field
-                    new_products.append(item)
-                print(f"Update: Page {page_number}: Extracted {len(extracted_data)} products.")
-                
-                # Add products in CSV file
-                if test_mode:
-                    configs.custom_csv_writer(
-                        file_to_be_written=test_file,
-                        headers=headers,
-                        products=new_products
-                        )
-                    print(type(result), type(result.extracted_content), type(extracted_data), type(new_products))
-                    product_count = product_count + len(new_products)
-                    crawled_page_count += 1
-                    print(f"Update: Page {page_number}: Written {len(new_products)} new products to database. Total written: {product_count}")
-                    print(f"TEST MODE SUCCESSFUL. WAIT FOR FINAL LOG")
+                if not result.success:
+                    print("STATUS: CRAWLING ERROR!!")
                     break
-                
                 else:
-                    configs.custom_csv_writer(
-                        file_to_be_written=main_file,
-                        headers=headers,
-                        products=new_products
-                        )
-                # Count of total products extracted & written
-                product_count = product_count + len(new_products)
-                print(f"Update: Page {page_number}: Written {len(new_products)} new products to database. Total written: {product_count}")
+                    print("STATUS: CRAWLING SUCCESSFUL. PROCESSING OUTPUT.")
+                    
+                    ## OUTPUT PIPELINE SHOULD BE MADE TASK AGNOSTIC. OUTPUT FUNCTION WILL TAKE THE RESULT AND DO THE REST.
+                    extracted_data = json.loads(result.extracted_content)
+                    # Stop if no products found
+                    if not extracted_data:
+                        print(f"No products found in Page {page_number}.")
+                        break 
                 
-                # Proceed to next page
-                page_number += 1
-                crawled_page_count += 1
-                # Delay Log
-                print(f"Proceeding to next page after {delay_time} seconds...")
-                await asyncio.sleep(delay_time) # Be polite and avoid overwhelming the server
+                    # Append new unique products
+                    new_products = [] 
+                    for item in extracted_data:
+                        new_products.append(item)
+                        print(f"Update: Page {page_number}: Extracted {len(extracted_data)} products.")
+                
+                        # Add products in CSV file
+                        if test_mode:
+                            configs.custom_csv_writer(
+                            file_to_be_written=test_file,
+                            products=new_products
+                            )
+                            product_count = product_count + len(new_products)
+                            crawled_page_count += 1
+                            print(f"Update: Page {page_number}: Written {len(new_products)} new products to database. Total written: {product_count}")
+                            print(f"TEST MODE SUCCESSFUL. WAIT FOR FINAL LOG")
+                            break
+                
+                        else:
+                            configs.custom_csv_writer(
+                            file_to_be_written=main_file,
+                            products=new_products
+                            )
+                            # Count of total products extracted & written
+                            product_count = product_count + len(new_products)
+                            print(f"Update: Page {page_number}: Written {len(new_products)} new products to database. Total written: {product_count}")
+                
+                            # Proceed to next page
+                            page_number += 1
+                            crawled_page_count += 1
+                            # Delay Log
+                            print(f"Proceeding to next page after {delay_time} seconds...")
+                            await asyncio.sleep(delay_time) # Be polite and avoid overwhelming the server
 
     
-    finally: 
-        # Estimated Product Count (CHECK LOG IN CASE OF DISCREPENCY)
+    finally:
+        print("CRAWLING COMPLETE.")
+        # SHOULD BE TASK-AGNOSTIC ::: Estimated Product Count (CHECK LOG IN CASE OF DISCREPENCY)
         estimated_product_count = (
             "0" if crawled_page_count == 0
             else "<=20" if crawled_page_count == 1
             else "20+" if crawled_page_count == 2
             else f"{(crawled_page_count - 1) * 20}+"
             )
-        # FINAL LOG
-        print(f"CRAWLING COMPLETE for Crawl no: {crawl_number}/{len(urls_to_crawl)-1}, Category: {category}. Crawled {crawled_page_count} Pages.")
-        print(f"Total {product_count} Products Added to Database.")
         print(f"ESTIMATED PRODUCT COUNT: {estimated_product_count}.\nCHECK LOG IN CASE OF DISCREPENCY.")
+        
+        # FINAL LOG
+        print(f"Crawl no: {crawl_number}/{len(urls_to_crawl)-1}, Category: {configs.CATEGORY}. Crawled {crawled_page_count} Pages.")
+        print(f"Total {product_count} Products Added to Database.")
+        
 
 async def main():
     await crawl_products(test_mode=test_mode)
